ee4cc9b863f49212359501732fe9e201557a5f9f
neil <neil@LAPTOP-NEIL.cantv.net>
2018-03-05 23:28:13 -0400

    prueba de fotos baner

--- resources/views/slider.blade.php
+++ resources/views/slider.blade.php
@@ -1,19 +1,19 @@
     <!-- Featured Slider | START -->
     <div id="featured">
         <div class="slider">
-            <div class="item" style="background-image:url(base-hotel/preview/images/hero-location-1800.jpg);">
+            <div class="item" style="background-image:url(base-hotel/preview/images/HotelHippocampus-5.jpg);">
                 <div class="details">
                     <div class="title"><span>Absolute Waterfront Luxury</span></div>
                     <div class="buttoncontainer"><a href="about.html" class="button"><span data-hover="Explore The Hotel">Explore The Hotel</span></a></div>
                 </div>
             </div>
-            <div class="item" style="background-image:url(base-hotel/preview/images/hero-accommodation-1800.jpg);">
+            <div class="item" style="background-image:url(base-hotel/preview/images/HotelHippocampus-4.jpg);">
                 <div class="details">
                     <div class="title"><span>Modernas y Cómodas Habitaciones</span></div>
                     <div class="buttoncontainer"><a href="{{ route('accommodation')}}" class="button"><span data-hover="Ver Habitaciones">Ver Habitaciones</span></a></div>
                 </div>
             </div>
-            <div class="item" style="background-image:url(base-hotel/preview/images/hero-restaurant-1800.jpg);">
+            <div class="item" style="background-image:url(base-hotel/preview/images/HotelHippocampus-2.jpg);">
                 <div class="details">
                     <div class="title"><span>A Gourmet Dining Experience</span></div>
                     <div class="buttoncontainer"><a href="restaurant.php" class="button"><span data-hover="Read The Menu">Read The Menu</span></a></div>
