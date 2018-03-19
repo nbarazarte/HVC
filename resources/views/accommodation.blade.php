b8b7c85805a2d5e9e57c6d2bbdc646e7bad28c0c
jaennie <jaennie@debian-jaennie.cantv.net>
2018-03-18 21:14:49 -0400

    2 ambientes otra vez

--- resources/views/accommodation.blade.php
+++ resources/views/accommodation.blade.php
@@ -58,7 +58,7 @@
                     <div class="imgcontainer"><img alt="" src="{{ asset('base-hotel/preview/images/duplex.jpg')}}" width="1200" height="400" /></div>
                     <div class="details">
                         <a href="{{ route('duplex')}}">
-                            <h3 class="title"> Duplex<br />
+                            <h3 class="title"> Duplex 2 Ambientes<br />
                             <span>Stay from $240 per night</span></h3>
                             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh.</p>
                             <div class="button"><span data-hover="Ver más">Ver más</span></div>
